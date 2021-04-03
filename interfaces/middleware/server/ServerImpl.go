f4a35cae24bdedfc2cd704d11e24385a870849b7
Andrés Gaviria <andres_felipe_gaviria28@hotmail.com>
2021-04-03 16:01:29 -0500

    swagger se remueve para pruebas
    Se remueve swagger para unas pruebas

--- interfaces/middleware/server/ServerImpl.go
+++ interfaces/middleware/server/ServerImpl.go
@@ -4,12 +4,12 @@ import (
 	"log"
 	"os"
 	"ws_restaurant/application"
-	"ws_restaurant/docs"
+	//"ws_restaurant/docs"
 	"ws_restaurant/interfaces/middleware"
 
 	"github.com/gin-gonic/gin"
-	swaggerFiles "github.com/swaggo/files" // swagger embed files
-	ginSwagger "github.com/swaggo/gin-swagger"
+	//swaggerFiles "github.com/swaggo/files" // swagger embed files
+	//ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 type ServerImpl struct {
@@ -20,21 +20,21 @@ func InitServer() Server {
 	serverImpl := &ServerImpl{}
 	router := gin.Default()
 	router.Use(middleware.CORSMiddleware())
-	swaggerDocs()
-	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	//swaggerDocs()
+	//router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	application.InitRestaurantController(router)
 	serverImpl.router = router
 	return serverImpl
 }
 
-func swaggerDocs() {
+/*func swaggerDocs() {
 	docs.SwaggerInfo.Title = os.Getenv("SWAGGER_TITLE")
 	docs.SwaggerInfo.Description = os.Getenv("SWAGGER_DESCRIPTION")
 	docs.SwaggerInfo.Version = os.Getenv("SWAGGER_VERSION")
 	docs.SwaggerInfo.Host = os.Getenv("SWAGGER_HOST")
 	docs.SwaggerInfo.BasePath = os.Getenv("SWAGGER_BASEPATH")
 	docs.SwaggerInfo.Schemes = []string{"http", "https"}
-}
+}*/
 
 func (api *ServerImpl) RunServer() {
 	appPort := os.Getenv("PORT")
