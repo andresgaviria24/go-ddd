2134f462ccbdb1a9f91c8380ae81636036964120
Andrés Gaviria <andres_felipe_gaviria28@hotmail.com>
2021-04-03 21:54:44 -0500

    ajuste de librerías para bd
    Se ajusta librerías para conexión

--- WS_Restaurant/go.mod
+++ WS_Restaurant/go.mod
@@ -3,11 +3,10 @@ module ws_restaurant
 go 1.15
 
 require (
-	github.com/HdrHistogram/hdrhistogram-go v1.0.1 // indirect
 	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751
-	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.6.3
-	github.com/jinzhu/gorm v1.9.16
+	github.com/go-sql-driver/mysql v1.5.0 // indirect
+	github.com/jinzhu/gorm v1.9.16 // indirect
 	github.com/joho/godotenv v1.3.0
 	github.com/opentracing/opentracing-go v1.2.0
 	github.com/swaggo/files v0.0.0-20190704085106-630677cd5c14
--- WS_Restaurant/infrastructure/persistence/DbHelper.go
+++ WS_Restaurant/infrastructure/persistence/DbHelper.go
@@ -4,8 +4,8 @@ import (
 	"os"
 	"ws_restaurant/infrastructure/repository"
 
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
 )
 
 type DbHelper struct {
@@ -16,7 +16,7 @@ type DbHelper struct {
 func InitDbHelper() (*DbHelper, error) {
 
 	dsn := os.Getenv("DB_USER") + ":" + os.Getenv("DB_PASSWORD") + "@tcp(" + os.Getenv("DB_HOST") + ":" + os.Getenv("DB_PORT") + ")/" + os.Getenv("DB_NAME") + "?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn))
+	db, err := gorm.Open("mysql", dsn)
 	if err != nil {
 		panic(err)
 	}
